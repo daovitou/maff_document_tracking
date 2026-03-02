5b120150b520a047d1726d46698a8bf13003d6b6
vitou dao <vitoudao@gmail.com>
2026-03-02 11:05:22 +0700

    add-icon

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <link rel="icon"  href="{{asset('assets/img/logo.png')}}">
     <title>{{ $title ?? config('app.name') }}</title>
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
