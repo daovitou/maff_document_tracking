2dc58c4fa31c65979089d6ecb1bfbf70241acd75
Sea Peng <mr.seapeng@gmail.com>
2026-05-10 21:01:38 +0700

    change some words

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon"  href="{{asset('assets/img/logo.png')}}">
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Document Tracking') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
